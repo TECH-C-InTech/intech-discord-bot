1b6ebba0005948e59b9750de18101afcfb48deab
KorRyu3 <146335193+KorRyu3@users.noreply.github.com>
2024-10-29 22:13:10 +0900

    feat: それっぽいdiscord botを作成

--- main.py
+++ main.py
@@ -1,6 +1,33 @@
-def main():
-    print("Hello from intech-discord-bot!")
+from logging import getLogger
+import os
 
+import discord
+from dotenv import load_dotenv
+
+logger = getLogger(__name__)
+
+load_dotenv()
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+CLIENT = discord.Client(intents=intents)
+
+@CLIENT.event
+async def on_ready():
+    print(f'We have logged in as {CLIENT.user}')
+
+@CLIENT.event
+async def on_message(message: discord.Message):
+    if message.author == CLIENT.user:
+        return
+
+    if message.content.startswith('!hello'):
+        await message.channel.send('Hello!')
 
 if __name__ == "__main__":
-    main()
+    token = os.getenv("DISCORD_BOT_TOKEN")
+    if token is None:
+        raise EnvironmentError("DISCORD_BOT_TOKEN is not set")
+
+    CLIENT.run(token)
