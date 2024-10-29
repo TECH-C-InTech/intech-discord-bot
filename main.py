58735cfe6f81572cfb40ebd641ff95960596c169
KorRyu3 <146335193+KorRyu3@users.noreply.github.com>
2024-10-29 23:21:09 +0900

    feat: emojiを追加/削除/変更したらイベントを発火させてメッセージを送信する

--- main.py
+++ main.py
@@ -1,15 +1,16 @@
-from logging import getLogger
+from logging import getLogger, basicConfig
 import os
 
 import discord
 from dotenv import load_dotenv
 
 logger = getLogger(__name__)
+basicConfig(level="INFO")
 
 load_dotenv()
 
 intents = discord.Intents.default()
-intents.message_content = True
+intents.emojis_and_stickers = True  # Required to receive on_guild_emojis_update
 
 CLIENT = discord.Client(intents=intents)
 
@@ -18,12 +19,44 @@ async def on_ready():
     print(f'We have logged in as {CLIENT.user}')
 
 @CLIENT.event
-async def on_message(message: discord.Message):
-    if message.author == CLIENT.user:
-        return
+async def on_guild_emojis_update(guild: discord.Guild, before: list[discord.Emoji], after: list[discord.Emoji]):
+    # :{emoji.name}: にすると、サーバー内で使えるカスタム絵文字を表示できる
 
-    if message.content.startswith('!hello'):
-        await message.channel.send('Hello!')
+    state = ""
+    message = ""
+
+    # logger.info("Diff:")
+    state = "Diff:" if set(before) != set(after) else ""
+    for emoji in set(after) - set(before):
+        message += f"Emoji: :{emoji.name}: added\n"
+        logger.info(message)
+
+    for emoji in set(before) - set(after):
+        message += f"Emoji: :{emoji.name}: removed\n"
+        logger.info(message)
+
+    # beforeにもあって、afterにもあるもの以外が変更されたもの
+    state = "Changed:" if set(before) & set(after) else ""
+    # logger.info("Changed:")
+    for emoji in set(before) & set(after):
+        if emoji in before and emoji in after:
+            if before[before.index(emoji)].name != after[after.index(emoji)].name:
+                message += f"Emoji: :{before[before.index(emoji)].name}: changed to :{after[after.index(emoji)].name}:\n"
+                logger.info(message)
+
+    if message != "":
+        logger.info("-" * 20)
+        logger.info(f"Guild: {guild.name}")
+        logger.info(state)
+        logger.info(message)
+
+    # send event
+    channel_name = os.getenv("EMOJI_LOG_CHANNEL_NAME")
+    channel = discord.utils.get(guild.text_channels, name=channel_name)
+    if channel is not None:
+        await channel.send(message)
+    else:
+        logger.error(f"{channel_name} channel not found")
 
 if __name__ == "__main__":
     token = os.getenv("DISCORD_BOT_TOKEN")
