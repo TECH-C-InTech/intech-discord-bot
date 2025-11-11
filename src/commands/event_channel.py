9464db6a28f37b4839be460438de4b13dd71d276
KorRyu3 <146335193+KorRyu3@users.noreply.github.com>
2025-11-11 22:38:59 +0900

    chore: 処理を直感的にする

--- src/commands/event_channel.py
+++ src/commands/event_channel.py
@@ -325,9 +325,14 @@ async def add_event_role_member_impl(
     if not await validate_role_safety(ctx, role):
         return
 
+
     # 同名のチャンネルがEVENT_CATEGORY_NAMEカテゴリーに存在するか確認
     # {index}-で始まるチャンネルを検索
-    event_channel = next((ch_name for ch_name in event_category.text_channels if ch_name.name.startswith(f"{role_name}-")), None)
+    event_channel = None
+    for ch_name in event_category.text_channels:
+        if ch_name.name.startswith(f"{role_name}-"):
+            event_channel = ch_name
+            break
 
     if not event_channel:
         await send_error_message(
